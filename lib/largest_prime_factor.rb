8d3d6f642702d19e7a9559d14a8f704e090dc434
babyshoes <dianecai@gmail.com>
2015-10-12 13:08:49 -0400

    done

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,37 @@
 # Enter your procedural solution here!
+require 'pry'
+# def largest_prime_factor(input)
+#   largest_prime_factor = 1
+#   [*2...(input/2).ceil].each do |num|
+#     largest_prime_factor = num if input%num == 0 && is_prime?(num) && largest_prime_factor < num
+#   end
+#   largest_prime_factor
+# end
+#
+# def is_prime?(num)
+#   ![*2..(num/2).ceil].any? {|n| num % n == 0}
+# end
+#
+# largest_prime_factor(50)
+
+
+def prime_factors(num)
+  factors = []
+  x = 2
+  while num > 1
+    while num % x == 0
+      factors << x
+      num /= x
+    end
+    x += 1
+  end
+  factors
+end
+
+def find_largest(arr)
+  arr.max
+end
+
+def largest_prime_factor(input)
+  find_largest prime_factors(input)
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,28 @@
 # Enter your object-oriented solution here!
+class LargestPrimeFactor
+  attr_accessor :input
+  def initialize(input)
+    @input = input
+  end
+
+  def prime_factors
+    factors = []
+    x = 2
+    while @input > 1
+      while @input % x == 0
+        factors << x
+        @input /= x
+      end
+      x += 1
+    end
+    factors
+  end
+
+  def find_largest(arr)
+    arr.max
+  end
+
+  def number
+    find_largest prime_factors
+  end
+end
